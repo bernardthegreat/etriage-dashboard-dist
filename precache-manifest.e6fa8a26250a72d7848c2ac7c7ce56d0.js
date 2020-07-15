0697ec8073b94bb4aaa6f2af5a7be7903e7a0df7
Bernard Gresola <gresola_bernard@yahoo.com>
2020-07-15 11:41:12 +0800

    Initial

---
+++ precache-manifest.e6fa8a26250a72d7848c2ac7c7ce56d0.js
@@ -0,0 +1,378 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45a64ccad5884d565221e3497d0c823",
+    "url": "/assets/bootstrap/css/bootstrap-grid.css"
+  },
+  {
+    "revision": "0ee1cd4a92a0b1648c41416a18a3d990",
+    "url": "/assets/bootstrap/css/bootstrap-grid.min.css"
+  },
+  {
+    "revision": "7e8d1142390c8e7f4690c125792517d5",
+    "url": "/assets/bootstrap/css/bootstrap-reboot.css"
+  },
+  {
+    "revision": "6a2ac181ef23890a6ec6edd05f61e6da",
+    "url": "/assets/bootstrap/css/bootstrap-reboot.min.css"
+  },
+  {
+    "revision": "b02604058889da17a92ea9d65cce6373",
+    "url": "/assets/bootstrap/css/bootstrap.css"
+  },
+  {
+    "revision": "78903984a106410080a529eed5a5a611",
+    "url": "/assets/bootstrap/css/bootstrap.min.css"
+  },
+  {
+    "revision": "b2ef0f9722d699154bf5686efdfbe337",
+    "url": "/assets/bootstrap/js/bootstrap.bundle.js"
+  },
+  {
+    "revision": "3648fa980c6623edee76d548305e5f8a",
+    "url": "/assets/bootstrap/js/bootstrap.bundle.min.js"
+  },
+  {
+    "revision": "1e5d4e586614aad0d297db64ba2b2171",
+    "url": "/assets/bootstrap/js/bootstrap.js"
+  },
+  {
+    "revision": "63534c9e99f47f6ef78f918a2c045247",
+    "url": "/assets/bootstrap/js/bootstrap.min.js"
+  },
+  {
+    "revision": "f4cd9354988ddbb952cc9760b2ffffef",
+    "url": "/assets/dist/css/bootstrap-grid.css"
+  },
+  {
+    "revision": "07f5043300a977c5b486b5872e9c9c6a",
+    "url": "/assets/dist/css/bootstrap-grid.min.css"
+  },
+  {
+    "revision": "db80ba7f1dbd333c13d7dcfd5df82c91",
+    "url": "/assets/dist/css/bootstrap-reboot.css"
+  },
+  {
+    "revision": "522e4ec96bebf2d79e37786091541e21",
+    "url": "/assets/dist/css/bootstrap-reboot.min.css"
+  },
+  {
+    "revision": "e04beb39d48812a0b98a877a3734d8fd",
+    "url": "/assets/dist/css/bootstrap.css"
+  },
+  {
+    "revision": "3afe15e976734d9daac26310110c4594",
+    "url": "/assets/dist/css/bootstrap.min.css"
+  },
+  {
+    "revision": "4d5e7e6ccbe7360bc74ee4930ac25477",
+    "url": "/assets/dist/js/bootstrap.bundle.js"
+  },
+  {
+    "revision": "7fd2f04e75bd7ab1a79d80cdd4c33085",
+    "url": "/assets/dist/js/bootstrap.bundle.min.js"
+  },
+  {
+    "revision": "6386470cd2491cbacf90825b3a65991c",
+    "url": "/assets/dist/js/bootstrap.js"
+  },
+  {
+    "revision": "6bea60c34c5db6797150610dacdc6bce",
+    "url": "/assets/dist/js/bootstrap.min.js"
+  },
+  {
+    "revision": "35a505ad1cd880510f3dd0fabcf8ec5b",
+    "url": "/assets/font-awesome/.npmignore"
+  },
+  {
+    "revision": "e5f4d96ed70c0c372ccf7a0d9841ba53",
+    "url": "/assets/font-awesome/HELP-US-OUT.txt"
+  },
+  {
+    "revision": "73d247690250bfbfbeb402d7a1c004f9",
+    "url": "/assets/font-awesome/README.md"
+  },
+  {
+    "revision": "4bb3dd721c4652feee0953261d329710",
+    "url": "/assets/font-awesome/css/font-awesome.css"
+  },
+  {
+    "revision": "a0e784c4ca94c271b0338dfb02055be6",
+    "url": "/assets/font-awesome/css/font-awesome.min.css"
+  },
+  {
+    "revision": "0d2717cd5d853e5c765ca032dfd41a4d",
+    "url": "/assets/font-awesome/fonts/FontAwesome.otf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/assets/font-awesome/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/assets/font-awesome/fonts/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/assets/font-awesome/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/assets/font-awesome/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/assets/font-awesome/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "7fec23ef95e17ff623af5dd5db0fc87d",
+    "url": "/assets/font-awesome/less/animated.less"
+  },
+  {
+    "revision": "1f57858ac2a7ab59b01088c778f5fb0a",
+    "url": "/assets/font-awesome/less/bordered-pulled.less"
+  },
+  {
+    "revision": "da355b2c884a067eedd979b445968147",
+    "url": "/assets/font-awesome/less/core.less"
+  },
+  {
+    "revision": "6799c9cf7edf54b7432cca85854b0abf",
+    "url": "/assets/font-awesome/less/fixed-width.less"
+  },
+  {
+    "revision": "62ab0957ccd3369ed07a6d7241e7b9c1",
+    "url": "/assets/font-awesome/less/font-awesome.less"
+  },
+  {
+    "revision": "ddaa02ea18934d4e03c9978f13311d5e",
+    "url": "/assets/font-awesome/less/icons.less"
+  },
+  {
+    "revision": "f588164c1a95535137186bbdb0d236c8",
+    "url": "/assets/font-awesome/less/larger.less"
+  },
+  {
+    "revision": "abcbffd56677821190cf4dc72f660dd9",
+    "url": "/assets/font-awesome/less/list.less"
+  },
+  {
+    "revision": "5b203b320a03575de5f369cf49b1e7c2",
+    "url": "/assets/font-awesome/less/mixins.less"
+  },
+  {
+    "revision": "fc4e5dec2b323ba7f687b0414a6faecd",
+    "url": "/assets/font-awesome/less/path.less"
+  },
+  {
+    "revision": "2088e18f446fcf2fe91e5293fc7f2847",
+    "url": "/assets/font-awesome/less/rotated-flipped.less"
+  },
+  {
+    "revision": "79f38eb1f3eea913308f49ab5cd663bf",
+    "url": "/assets/font-awesome/less/screen-reader.less"
+  },
+  {
+    "revision": "8b7e28e004c785a0a1a73afde547b9e7",
+    "url": "/assets/font-awesome/less/stacked.less"
+  },
+  {
+    "revision": "62803119d3ed114705a50717a53e2639",
+    "url": "/assets/font-awesome/less/variables.less"
+  },
+  {
+    "revision": "311e10e5a88718c5ecd88bbcf8b765fa",
+    "url": "/assets/font-awesome/package.json"
+  },
+  {
+    "revision": "79db4428659752f59630c4388bbaa174",
+    "url": "/assets/font-awesome/scss/_animated.scss"
+  },
+  {
+    "revision": "6e194951ee06dd4cde6f94ed2ec86e47",
+    "url": "/assets/font-awesome/scss/_bordered-pulled.scss"
+  },
+  {
+    "revision": "d4d62bafcfb00eb1b208acbf895a95a3",
+    "url": "/assets/font-awesome/scss/_core.scss"
+  },
+  {
+    "revision": "e6fac8be09e2d3dc38fe9248b2eb0472",
+    "url": "/assets/font-awesome/scss/_fixed-width.scss"
+  },
+  {
+    "revision": "e624b0b5cfd7ac3fcb4eefab15f983f6",
+    "url": "/assets/font-awesome/scss/_icons.scss"
+  },
+  {
+    "revision": "8b00a4d732107fc1844ea18314c3cff9",
+    "url": "/assets/font-awesome/scss/_larger.scss"
+  },
+  {
+    "revision": "c33b069275c5877a4b3f144684664bf5",
+    "url": "/assets/font-awesome/scss/_list.scss"
+  },
+  {
+    "revision": "f07164e3950c5dac464e3f5f9e14858d",
+    "url": "/assets/font-awesome/scss/_mixins.scss"
+  },
+  {
+    "revision": "123f2994215b526902be3fa0596c8d13",
+    "url": "/assets/font-awesome/scss/_path.scss"
+  },
+  {
+    "revision": "9a3f214edda562bf122802da5c686a12",
+    "url": "/assets/font-awesome/scss/_rotated-flipped.scss"
+  },
+  {
+    "revision": "b4b497bdd83f580fa3942390763cc270",
+    "url": "/assets/font-awesome/scss/_screen-reader.scss"
+  },
+  {
+    "revision": "638e8ae84e80a3428e9446578a7ed6a0",
+    "url": "/assets/font-awesome/scss/_stacked.scss"
+  },
+  {
+    "revision": "547c9f94a31ced29cbc7f029d700a639",
+    "url": "/assets/font-awesome/scss/_variables.scss"
+  },
+  {
+    "revision": "bc5096695c4092d06d77da5329254590",
+    "url": "/assets/font-awesome/scss/font-awesome.scss"
+  },
+  {
+    "revision": "fb1817b96c65b6477cb55fedf53e86d9",
+    "url": "/assets/jquery/jquery.js"
+  },
+  {
+    "revision": "b61aa6e2d68d21b3546b5b418bf0e9c3",
+    "url": "/assets/jquery/jquery.min.js"
+  },
+  {
+    "revision": "ac0bbd8679855592c4fae48fbd2e3b14",
+    "url": "/assets/jquery/jquery.slim.js"
+  },
+  {
+    "revision": "9e73c6c1b301724565d9937bf8e564a5",
+    "url": "/assets/jquery/jquery.slim.min.js"
+  },
+  {
+    "revision": "53a2a827114d665e0f2176ed58b5592f",
+    "url": "/assets/popper/esm/popper-utils.js"
+  },
+  {
+    "revision": "3a272f95640a683188ca9de2f53c9645",
+    "url": "/assets/popper/esm/popper-utils.min.js"
+  },
+  {
+    "revision": "0037eae019975be4c74716b879cfe29a",
+    "url": "/assets/popper/esm/popper.js"
+  },
+  {
+    "revision": "70d452caca7d5a79a11abbc477b32d0d",
+    "url": "/assets/popper/esm/popper.min.js"
+  },
+  {
+    "revision": "846b1780e9a1d8fbde360163c6030237",
+    "url": "/assets/popper/popper-utils.js"
+  },
+  {
+    "revision": "c5e0b705b2e6751e2953f651057df309",
+    "url": "/assets/popper/popper-utils.min.js"
+  },
+  {
+    "revision": "221afb263e2662bbfe961bf847d238c5",
+    "url": "/assets/popper/popper.js"
+  },
+  {
+    "revision": "28df78f4c5ef178ca2a3f3c6eed00226",
+    "url": "/assets/popper/popper.min.js"
+  },
+  {
+    "revision": "3b53956d4e2911d6cb9d1d7ac57724ae",
+    "url": "/assets/popper/umd/popper-utils.js"
+  },
+  {
+    "revision": "a614977b4a10a8a8323165e9ccf4d612",
+    "url": "/assets/popper/umd/popper-utils.min.js"
+  },
+  {
+    "revision": "701c1c4a6731e30b7b41a6425f4fc35a",
+    "url": "/assets/popper/umd/popper.js"
+  },
+  {
+    "revision": "ac1b2007a877aeed41d7d0ffe4a0ac9b",
+    "url": "/assets/popper/umd/popper.js.flow"
+  },
+  {
+    "revision": "0dd1c14014c608e71a095403f77bb633",
+    "url": "/assets/popper/umd/popper.min.js"
+  },
+  {
+    "revision": "636d8475dab94c744ee0",
+    "url": "/css/app.6543776e.css"
+  },
+  {
+    "revision": "e154e76cdb25d7f64e90",
+    "url": "/css/chunk-vendors.65e3d94f.css"
+  },
+  {
+    "revision": "f4cd9354988ddbb952cc9760b2ffffef",
+    "url": "/dist/css/bootstrap-grid.css"
+  },
+  {
+    "revision": "07f5043300a977c5b486b5872e9c9c6a",
+    "url": "/dist/css/bootstrap-grid.min.css"
+  },
+  {
+    "revision": "db80ba7f1dbd333c13d7dcfd5df82c91",
+    "url": "/dist/css/bootstrap-reboot.css"
+  },
+  {
+    "revision": "522e4ec96bebf2d79e37786091541e21",
+    "url": "/dist/css/bootstrap-reboot.min.css"
+  },
+  {
+    "revision": "e04beb39d48812a0b98a877a3734d8fd",
+    "url": "/dist/css/bootstrap.css"
+  },
+  {
+    "revision": "3afe15e976734d9daac26310110c4594",
+    "url": "/dist/css/bootstrap.min.css"
+  },
+  {
+    "revision": "4d5e7e6ccbe7360bc74ee4930ac25477",
+    "url": "/dist/js/bootstrap.bundle.js"
+  },
+  {
+    "revision": "7fd2f04e75bd7ab1a79d80cdd4c33085",
+    "url": "/dist/js/bootstrap.bundle.min.js"
+  },
+  {
+    "revision": "6386470cd2491cbacf90825b3a65991c",
+    "url": "/dist/js/bootstrap.js"
+  },
+  {
+    "revision": "6bea60c34c5db6797150610dacdc6bce",
+    "url": "/dist/js/bootstrap.min.js"
+  },
+  {
+    "revision": "e04bd4d461b057dd894e81c512cc7f7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "636d8475dab94c744ee0",
+    "url": "/js/app.3e913732.js"
+  },
+  {
+    "revision": "e154e76cdb25d7f64e90",
+    "url": "/js/chunk-vendors.6ad6d00d.js"
+  },
+  {
+    "revision": "4e757b30888158f5ec2040fa0b4c980a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
